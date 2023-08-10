2e5f6a37b319e9f8dab702b3787db374a20dce54
Julio Gomes <juliogomesj09@gmail.com>
2023-08-09 21:49:49 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
@@ -8,6 +9,11 @@ function styles(){
     .pipe(gulp.dest('./dist/css'));
 }
 
+function scripts(){
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 ///function images() {
     //return gulp.src('./src/images/**/*')
@@ -16,7 +22,8 @@ function styles(){
 }*/
 
 
-exports.default = gulp.parallel(styles,);
+exports.default = gulp.parallel(styles, scripts);
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
